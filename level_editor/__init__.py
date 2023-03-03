e59a7b4f93b41bb09841ff028c5a0283e3659ab1
Yvan Satyawan <y_satyawan@hotmail.com>
2023-03-03 15:49:35 +0100

    Fixed saving grid in the wrong location

--- level_editor/__init__.py
+++ level_editor/__init__.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 
-GRID_CONFIGS_FP = Path.cwd().parent / Path("grid_configs")
+GRID_CONFIGS_FP = Path(__file__).parents[1].resolve() / Path("grid_configs")
 GRID_CONFIGS_FP.mkdir(parents=True, exist_ok=True)
 
 __all__ = ["GRID_CONFIGS_FP"]
