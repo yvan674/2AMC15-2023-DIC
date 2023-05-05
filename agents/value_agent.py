854e67a3aea1ffc2749167df2329297574ad14af
plofknaapje <robinverhoef1007@gmail.com>
2023-05-05 16:25:25 +0200

    First working agent - value iteration

--- agents/value_agent.py
+++ agents/value_agent.py
@@ -2,106 +2,145 @@
 
 Chooses the best scoring value with no thought about the future.
 """
-import numpy as np
 from random import randint
+
+import numpy as np
+
 from agents import BaseAgent
 
 
 class ValueAgent(BaseAgent):
-    def __init__(self, agent_number, observation, gamma, theta=0.001):
+    def __init__(self, agent_number: int, gamma: float, theta=0.001):
         """
+        Set agent parameters.
 
         Args:
             agent_number: The index of the agent in the environment.
+            gamma: loss rate.
+            theta: minimal change.
         """
-
         super().__init__(agent_number)
-        rows, cols = observation.shape
-        self.obs = observation
         self.gamma = gamma
         self.theta = theta
 
-        self.states = [(i, j) for i in range(rows) for j in range(cols)
-                       if self.obs[i, j] in (0, 3, 4)]
-        print(len(self.states))
-        self.values = {state: 0 for state in self.states}
-        self.policy = self.value_iteration()
-
-    def value_iteration(self) -> dict:
-        i = 0
-        delta = self.theta * 2
-        while delta > self.theta:
-            delta = 0
-            for state in self.states:
-                v = self.values[state]
-                self.values[state] = self.max_value(state)
-                delta = max(delta, abs(v - self.values[state]))
-            
-            print(f"Iteration {i}: delta = {delta}")
-            i += 1
-        
-        return {state: self.best_action(state) for state in self.states}
- 
-    def max_value(self, state: tuple) -> float:
-        new_values = []
-        for action in (0, 1, 2, 3):
-            total = 0
-            for neighbor in neighbor_states(state):
-                for reward in [0, 1]:
-                    curr_prob = self.prob(state, action, neighbor, reward)
-                    if curr_prob != 0:
-                        total += curr_prob * (reward + self.gamma * self.values[neighbor])
-
-            new_values.append(total)
-        return max(new_values)
-    
-    def best_action(self, state: tuple) -> int:
-        if self.obs[state] == 4:
-            return 4
-        
-        best_value = 0
-        best_action = None
-        for action in (0, 1, 2, 3):
-            total = 0
-            for neighbor in neighbor_states(state):
-                for reward in [0, 1]:
-                    curr_prob = self.prob(state, action, neighbor, reward)
-                    if curr_prob != 0:
-                        total += curr_prob * (reward + self.gamma * self.values[neighbor])
-
-            if total > best_value:
-                best_value = total
-                best_action = action
-        
-        return best_action
-        
     def process_reward(self, observation: np.ndarray, reward: float):
         pass
 
     def take_action(self, observation: np.ndarray, info: None | dict) -> int:
-        x, y = info["agent_pos"][self.agent_number]
-        # Check each neighboring cell if there is any dirt there
-        return self.policy[(x, y)]
+        """
+        Default method which decides an action based on the environment.
 
+        Args:
+            observation: current environment.
+            info: situation in current environment.
 
-    def prob(self, state: tuple, action: int, new_state: tuple, reward: int) -> float:
-        if self.obs[state] == 4:
-            return float(0)
-        
-        neighbor = neighbor_states(state)[action]
+        Returns:
+            int: action to be taken.
+        """
 
-        if self.obs[neighbor] == 0:
-            return float(new_state == neighbor and reward == 0)
-        if self.obs[neighbor] in (1, 2):
-            return float(new_state == state and reward == 0)
-        if self.obs[neighbor] == 3:
-            return float(new_state == state and reward == 1)
-        if self.obs[neighbor] == 4:
-            return float(new_state == state and reward == 0)
-        
-        return float(0)
+        # Setup for VI
+        self.obs = observation
+        cols, rows = observation.shape
+        # Col - row
+        self.states = [
+            (i, j)
+            for i in range(cols)
+            for j in range(rows)
+            if observation[i, j] in (0, 3, 4)
+        ]
+        self.chargers = [
+            (i, j) for i in range(cols) for j in range(rows) if observation[i, j] == 4
+        ]
 
-def neighbor_states(state: tuple) -> list:
-    x, y = state
-    # Actions 0, 1, 2, 3, 4
-    return [(x, y+1), (x, y-1), (x-1, y), (x+1, y), (x, y)]
+        # Initialize self.values based on situation in the room
+        if 3 in self.obs:  # Still dirt to be cleaned
+            self.values = {
+                state: (1 if self.obs[state] == 3 else 0) for state in self.states
+            }
+            for charger in self.chargers:
+                self.values[charger] = -1
+            self.cleaning = True
+        else:  # Go to charging station
+            self.values = {
+                state: (1 if state in self.chargers else 0) for state in self.states
+            }
+            self.cleaning = False
+
+        self.value_iteration()
+
+        # Use self.values to pick best action
+        state = info["agent_pos"][self.agent_number]
+        return self.generate_policy(state)
+
+    def value_iteration(self):
+        """
+        Value iteration implementation. Keep optimizing V until change is
+        smaller than theta. self.values is updated in place.
+        """
+        delta = 2 * self.theta  # Initial delta to ensure loop starts
+        while delta > self.theta:
+            delta = 0
+            for state in self.states:
+                old_value = self.values[state]
+                self.values[state] = self.max_action(state, True)[0]
+                delta = max(delta, abs(self.values[state] - old_value))
+
+    def max_action(self, state: tuple[int, int]) -> tuple[float, int]:
+        """
+        Try all actions from the current state and determine the action with
+        the highest expected value.
+
+        Args:
+            state: current state of the agent.
+
+        Returns:
+            tuple: value, action pair with the highest value.
+        """
+        value_action = []
+        for action in (0, 1, 2, 3, 4):
+            new_state = self.action_outcome(state, action)
+            if new_state == (-1, -1):
+                continue
+            elif self.obs[new_state] == 3 and self.cleaning:
+                value = 1
+            elif self.obs[new_state] == 4 and not self.cleaning:
+                value = 1
+            elif self.obs[new_state] == 4 and self.cleaning:
+                value = 0
+            else:
+                value = self.values[new_state] * self.gamma
+            value_action.append((value, action))
+
+        return max(value_action)
+
+    def action_outcome(self, state, action) -> tuple:
+        """
+        Determines the result of a certain action when taken in a certain place.
+
+        Args:
+            state: current state of the agent.
+            action: action to be taken.
+
+        Returns:
+            tuple: resulting state. (-1, -1) if the action is illegal.
+        """
+        # Col - row
+        x, y = state
+        match action:
+            case 0:  # Down
+                new_state = (x, y + 1)
+            case 1:  # Up
+                new_state = (x, y - 1)
+            case 2:  # Left
+                new_state = (x - 1, y)
+            case 3:  # Right
+                new_state = (x + 1, y)
+            case 4:  # Stand still
+                new_state = state
+        if new_state in self.states:
+            return new_state
+        else:
+            return (-1, -1)
+
+    def generate_policy(self, state):
+        return self.max_action(state)[1]
--- train.py
+++ train.py
@@ -12,75 +12,102 @@ from pathlib import Path
 from tqdm import trange
 
 try:
-    from world import Environment
+    from agents.greedy_agent import GreedyAgent
 
     # Add your agents here
     from agents.null_agent import NullAgent
-    from agents.greedy_agent import GreedyAgent
     from agents.random_agent import RandomAgent
     from agents.value_agent import ValueAgent
+    from world import Environment
 except ModuleNotFoundError:
-    from os import path
-    from os import pardir
     import sys
+    from os import pardir, path
 
-    root_path = path.abspath(path.join(
-        path.join(path.abspath(__file__), pardir), pardir)
+    root_path = path.abspath(
+        path.join(path.join(path.abspath(__file__), pardir), pardir)
     )
 
     if root_path not in sys.path:
         sys.path.extend(root_path)
 
-    from world import Environment
+    from agents.greedy_agent import GreedyAgent
 
     # Add your agents here
     from agents.null_agent import NullAgent
-    from agents.greedy_agent import GreedyAgent
     from agents.random_agent import RandomAgent
     from agents.value_agent import ValueAgent
-
+    from world import Environment
 
 
 def parse_args():
     p = ArgumentParser(description="DIC Reinforcement Learning Trainer.")
-    p.add_argument("GRID", type=Path, nargs="+",
-                   help="Paths to the grid file to use. There can be more than "
-                        "one.")
-    p.add_argument("--no_gui", action="store_true",
-                   help="Disables rendering to train faster")
-    p.add_argument("--sigma", type=float, default=0.,
-                   help="Sigma value for the stochasticity of the environment.")
-    p.add_argument("--fps", type=int, default=30,
-                   help="Frames per second to render at. Only used if "
-                        "no_gui is not set.")
-    p.add_argument("--iter", type=int, default=1000,
-                   help="Number of iterations to go through.")
-    p.add_argument("--random_seed", type=int, default=0,
-                   help="Random seed value for the environment.")
-    p.add_argument("--out", type=Path,
-                   help="Where to save training results.")
+    p.add_argument(
+        "GRID",
+        type=Path,
+        nargs="+",
+        help="Paths to the grid file to use. There can be more than " "one.",
+    )
+    p.add_argument(
+        "--no_gui", action="store_true", help="Disables rendering to train faster"
+    )
+    p.add_argument(
+        "--sigma",
+        type=float,
+        default=0.0,
+        help="Sigma value for the stochasticity of the environment.",
+    )
+    p.add_argument(
+        "--fps",
+        type=int,
+        default=30,
+        help="Frames per second to render at. Only used if " "no_gui is not set.",
+    )
+    p.add_argument(
+        "--iter", type=int, default=1000, help="Number of iterations to go through."
+    )
+    p.add_argument(
+        "--random_seed",
+        type=int,
+        default=0,
+        help="Random seed value for the environment.",
+    )
+    p.add_argument("--out", type=Path, help="Where to save training results.")
 
     return p.parse_args()
 
 
-def main(grid_paths: list[Path], no_gui: bool, iters: int, fps: int,
-         sigma: float, out: Path, random_seed: int):
+def main(
+    grid_paths: list[Path],
+    no_gui: bool,
+    iters: int,
+    fps: int,
+    sigma: float,
+    out: Path,
+    random_seed: int,
+):
     """Main loop of the program."""
 
     for grid in grid_paths:
         # Set up the environment and reset it to its initial state
-        env = Environment(grid, no_gui, n_agents=1, agent_start_pos=None,
-                          sigma=sigma, target_fps=fps, random_seed=random_seed)
+        env = Environment(
+            grid,
+            no_gui,
+            n_agents=1,
+            agent_start_pos=None,
+            sigma=sigma,
+            target_fps=fps,
+            random_seed=random_seed,
+        )
         obs, info = env.get_observation()
 
         # Set up the agents from scratch for every grid
         # Add your agents here
         agents = [
-                  # NullAgent(0),
-                  # GreedyAgent(0),
-                  # RandomAgent(0),
-                  ValueAgent(0, obs, gamma=0.9)
-                  ]
+            # NullAgent(0),
+            # GreedyAgent(0),
+            # RandomAgent(0),
+            ValueAgent(0, gamma=0.9)
+        ]
 
         # Iterate through each agent for `iters` iterations
         for agent in agents:
@@ -101,8 +128,14 @@ def main(grid_paths: list[Path], no_gui: bool, iters: int, fps: int,
             Environment.evaluate_agent(grid, [agent], 1000, out, 0.2)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
-    main(args.GRID, args.no_gui, args.iter, args.fps, args.sigma, args.out,
-         args.random_seed)
+    main(
+        args.GRID,
+        args.no_gui,
+        args.iter,
+        args.fps,
+        args.sigma,
+        args.out,
+        args.random_seed,
+    )
