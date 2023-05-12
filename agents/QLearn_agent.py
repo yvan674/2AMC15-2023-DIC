5743bfd93e8460dbea5372f5ce5f909049813011
Tim Doughty <doughtytimothy@aol.com>
2023-05-12 11:29:41 +0200

    Dirt Function

--- agents/QLearn_agent.py
+++ agents/QLearn_agent.py
@@ -4,7 +4,7 @@ This is an agent that takes a random action from the available action space.
 """
 from random import randint
 import numpy as np
-
+import math
 from agents import BaseAgent
 
 
@@ -26,11 +26,91 @@ class QLearnAgent(BaseAgent):
         self.epsilon = epsilon
         self.alpha = alpha
         self.Q = None
+        self.dirtGrid = np.zeros(4)
 
     def process_reward(self, observation: np.ndarray, reward: float):
         pass
 
     # TODO: Dirt check function
+    def dirt_function(self, observation: np.ndarray, state):
+        #check which quarter
+        height = observation.shape(0)
+        width = observation.shape(1)
+        if state[0] < height/2:
+            if state[1] < width/2:
+                quarter = 0
+            else:
+                quarter = 1
+        else:
+            if state[1] < width/2:
+                quarter = 2
+            else:
+                quarter = 3
+
+        #check if already dirt free
+        if self.dirtGrid(quarter) == 1:
+            return dirt_byte_converter(self.dirtGrid)
+        
+        dirty = False
+
+
+
+        if quarter == 1:
+            for i in range(0, math.floor(height/2)):
+                for j in range(0, math.floor(width/2)):
+                    if observation[i][j] == 3:
+                        dirty = True
+                        break
+                if dirty:
+                    break
+
+        
+        if quarter == 2:
+            for i in range(0, math.floor(height/2)):
+                for j in range(math.ceil(width/2), width):
+                    if observation[i][j] == 3:
+                        dirty = True
+                        break
+                if dirty:
+                    break
+
+        if quarter == 3:
+            for i in range(math.ceil(height/2), height):
+                for j in range(0, math.floor(width/2)):
+                    if observation[i][j] == 3:
+                        dirty = True
+                        break
+                if dirty:
+                    break
+                
+        if quarter == 4:
+            for i in range(math.ceil(height/2), height):
+                for j in range(math.ceil(width/2), width):
+                    if observation[i][j] == 3:
+                        dirty = True
+                        break
+                if dirty:
+                    break
+        
+        if dirty == False:
+            self.dirtGrid[quarter] = 1
+        return dirt_byte_converter(self.dirtGrid)
+
+        #check if there is dirt
+        #update dirt 
+        
+    def dirt_byte_converter(dirtGrid):
+        number = 0
+        if dirtGrid[0] == 1:
+            number += 1
+        if dirtGrid[1] == 1:
+            number += 2
+        if dirtGrid[2] == 1:
+            number += 4
+        if dirtGrid[3] == 1:
+            number += 8
+        return number
+
     # TODO: Get new state to update Q table
     # TODO: Q learning implementation
     def take_action(self, observation: np.ndarray, info: None | dict) -> int:
